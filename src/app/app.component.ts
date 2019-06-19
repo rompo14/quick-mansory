dd4855638fa85549f7d10536023b322063787237
Roman Popov <rompo14@gmail.com>
2019-06-19 22:03:39 +0800

    mvc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
   title = 'novator';
+
+  onSubmit(e) {
+    e.preventDefault();
+    const mansory = document.getElementsByClassName('mansory')[0];
+    console.log(mansory);
+    for (let i = 0; i < e.target.items.value; i++) {
+      mansory.insertAdjacentHTML('beforeend', '<div class="item" style="height: ' + (Math.floor(Math.random() * (400 - 200)) + 200) + 'px"></div>');
+    }
+  }
 }
